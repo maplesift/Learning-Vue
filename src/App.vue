e06648f1c6a11536a653d009fbbf0f017d955351
maplesift <a23050716@gmail.com>
2024-10-02 16:52:29 +0800

    new file:   src/App.vue 	new file:   src/main.ts

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <!--html-->
+    <div class="app">
+        <h1>hello!</h1>
+    </div>
+</template>
+
+<script lang="ts">
+    //js and ts
+    export default{
+        name: 'App' //組件名
+    }
+</script>
+
+<style>
+    /*樣式 */
+    .app{
+        background-color: #ddd;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
